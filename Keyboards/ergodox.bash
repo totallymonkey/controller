6bfb1ff6f2c5def69c7754d1e1e01cfe624a82c3
Andrew Davis <andrewnd@gmail.com>
2017-04-28 10:16:14 -0700

    Update bash script to build updated layout files

--- Keyboards/ergodox.bash
+++ Keyboards/ergodox.bash
@@ -25,7 +25,7 @@ BaseMap="scancode_map leftHand slave1 rightHand"
 # This is the default layer of the keyboard
 # NOTE: To combine kll files into a single layout, separate them by spaces
 # e.g.  DefaultMap="mylayout mylayoutmod"
-DefaultMap="mdergo1Overlay lcdFuncMap"
+DefaultMap="myergodox/mdergo1Overlay myergodox/lcdFuncMap"
 
 # This is where you set the additional layers
 # NOTE: Indexing starts at 1
@@ -33,10 +33,10 @@ DefaultMap="mdergo1Overlay lcdFuncMap"
 # e.g.  PartialMaps[1]="layer1 layer1mod"
 #       PartialMaps[2]="layer2"
 #       PartialMaps[3]="layer3"
-PartialMaps[1]="iced_func"
-PartialMaps[2]="iced_numpad"
-
-
+PartialMaps[1]="colemakMod myergodox/lcdFuncMap"
+PartialMaps[2]="iced_func myergodox/lcdFuncMap"
+PartialMaps[3]="iced_numpad myergodox/lcdFuncMap"
+PartialMaps[4]="myergodox/iced_prog myergodox/lcdFuncMap"
 
 ##########################
 # Advanced Configuration #
